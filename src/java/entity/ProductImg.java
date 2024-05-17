d8aa5f058138cc8fc3875ee87137d2c8668dae29
longvnhe181555 <longvnhe181555@fpt.edu.vn>
2024-05-17 23:32:20 +0700

    Replace img as array lsit

---
+++ src/java/entity/ProductImg.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+public class ProductImg {
+
+    int iid;
+    String imgpath;
+    int pid;
+
+    public int getIid() {
+        return iid;
+    }
+
+    public void setIid(int iid) {
+        this.iid = iid;
+    }
+
+    public String getImgpath() {
+        return imgpath;
+    }
+
+    public void setImgpath(String imgpath) {
+        this.imgpath = imgpath;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+}
