8cd07b6ed36a6deda2d4f0188b92424f6d83053a
haidang0k <ledang00002k@gmail.com>
2024-06-24 23:28:04 +0700

    Admin dashboard (first time)

---
+++ src/java/dal/OrderDBContext.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import entity.IEntity;
+import entity.Order;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class OrderDBContext extends DBContext {
+
+    @Override
+    public ArrayList list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insert(IEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(IEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(IEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public IEntity get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public List<Order> getOrdersByDate(Date date) {
+        List<Order> orders = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM [order] WHERE [order_date] = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setDate(1, new java.sql.Date(date.getTime()));
+            ResultSet rs = stm.executeQuery();
+            Order order;
+            while (rs.next()) {
+                order = new Order();
+                order.setTotalAmount(rs.getInt("total_amount"));
+                order.setStatus(rs.getInt("status"));
+                orders.add(order);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return orders;
+    }
+
+    public HashMap<Integer, Integer> getTotalAmountByYear(int year) {
+        HashMap<Integer, Integer> result = new HashMap();
+        try {
+            String sql = "SELECT DATEPART(month , [order_date]) as month , SUM(total_amount) as total_amount FROM [order]\n"
+                    + "WHERE DATEPART(year , [order_date]) = ?  group by DATEPART(month , [order_date])";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, year);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                int totalAmount = rs.getInt("total_amount");
+                int month = rs.getInt("month");
+                result.put(month, totalAmount);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+}
---
+++ src/java/entity/Order.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class Order {
+    private int orderId;
+    private Account account;
+    private Date date;
+    private int totalAmount;
+    private int status;
+    private String note;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(int totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+    
+    
+}
